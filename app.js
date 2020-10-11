c4b31de7d4f25b622980ab7cc126291727db01be
Jonathan Strauss <office@jstrauss.at>
2020-10-11 17:03:03 +0200

    Changes Webserver

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ app.get("/", (req, res) => {
 
     res.sendFile(__dirname + "/static/index.html");
 
+    let date = new Date();
+
     let ip = req.headers['x-forwarded-for'] ||
         req.connection.remoteAddress ||
         req.socket.remoteAddress ||
         (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    console.log(ip);
+    ip = ip.replace("::ffff:", "");
+    console.log(date.toString() + "  " + ip);
 });
 
 
